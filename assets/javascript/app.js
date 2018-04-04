b8f80fc064294922e20ae830cc907bc33605a395
jharnois444 <jharnois444@gmail.com>
2018-04-04 19:35:52 -0400

    session page has working embedded video chat

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,8 @@
   
-  // Initialize Firebase
+//  $( document ).ready(function(){
+
+
+ // Initialize Firebase
   var config = {
     apiKey: "AIzaSyB3_X63IybWvheUGhH08x2FCuRWsCCNqa4",
     authDomain: "web-quickstart-d9627.firebaseapp.com",
@@ -10,11 +13,33 @@
   };
   firebase.initializeApp(config);
 
+  
 var database = firebase.database()
+
+$("button").on("click", function(){
+
 var name = $("#name").val().trim()
 var email = $("#email").val().trim()
 var username = $("#username").val().trim()
 var password = $("#password").val().trim()
 var retypepassword = $("#retypePassword").val().trim()
 var proficiencies = $("#proficiencies").val().trim()
-var bio = $("#bio").val().trim()
+var bio = $("#bio").val();
+
+console.log(name);
+alert ("button working");
+
+database.ref("/users").push({
+  name:name,
+  email:email,
+  username:username,
+  password:password,
+  retypepassword:retypepassword,
+  proficiencies:proficiencies,
+  bio:bio
+});
+
+});
+
+// });
+ 
