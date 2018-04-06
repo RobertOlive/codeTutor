dbffe752a9c1163d2bfd20713fe809cf2b1f9dd2
Robert Olive <32848494+RobertOlive@users.noreply.github.com>
2018-04-05 20:07:25 -0400

    url params

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -139,44 +139,24 @@ database.ref().on("value", function(snap) {
   }
 
 })
-
 var gruCode = Math.floor(Math.random() * 100000)+"codeTutor"
-
-var mainCode = gruCode.toString()
-console.log(mainCode)
-
+// console.log("this is my random number " + gruCode);
+// var mainCode = gruCode.toString()
+// console.log(mainCode)
+var url = getAllUrlParams().gruCode
+console.log(url);
 $(document).on("click", ".email", function () {
   // send email function
-  window.location = "session.html"
+  window.location = "session.html?gruCode="+gruCode
+
   console.log($(this).text())
-  window.open('mailto:'+$(this).text()+'?subject="Your codeTutor Code!"&body="Attached is your videochat code:"'+mainCode+'');
+  window.open('mailto:'+$(this).text()+'?subject="Your codeTutor Code!"&body="Attached is your videochat code:"' + gruCode + '');
 })
 
 
 // END OF PROFILE BROWSER
 
 // ACCOUNT PAGE
-
-// var signupFn = function() {
-//    var userObj = {
-//     name: name,
-//     email: email,
-//     user: user,
-//     pass: pass,
-//     rePass: rePass,
-//     prof: prof,
-//     bio: bio  
-//    }
-//    if (userObj) {
-//      $("#tbodyAccount").html("<tr><td>" + name + 
-//                             "</td><td>" + email + 
-//                             "</td><td>" + user +
-//                             "</td><td>" + prof +
-//                             "</td><td>" + bio + "</td></tr>")
-//    }
-// }
-
-
 
 // END OF ACCOUNT PAGE
 
@@ -198,7 +178,71 @@ $(document).on("click", ".email", function () {
       embed = new Gruveo.Embed("myembed", {
         responsive: 1,
         embedParams: {
-          code: mainCode
+          code: url
         }
       });
     }
+    function getAllUrlParams(url) {
+
+      // get query string from url (optional) or window
+      var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+    
+      // we'll store the parameters here
+      var obj = {};
+    
+      // if query string exists
+      if (queryString) {
+    
+        // stuff after # is not part of query string, so get rid of it
+        queryString = queryString.split('#')[0];
+    
+        // split our query string into its component parts
+        var arr = queryString.split('&');
+    
+        for (var i=0; i<arr.length; i++) {
+          // separate the keys and the values
+          var a = arr[i].split('=');
+    
+          // in case params look like: list[]=thing1&list[]=thing2
+          var paramNum = undefined;
+          var paramName = a[0].replace(/\[\d*\]/, function(v) {
+            paramNum = v.slice(1,-1);
+            return '';
+          });
+    
+          // set parameter value (use 'true' if empty)
+          var paramValue = typeof(a[1])==='undefined' ? true : a[1];
+    
+          // // (optional) keep case consistent
+          // paramName = paramName.toLowerCase();
+          // paramValue = paramValue.toLowerCase();
+    
+          // if parameter name already exists
+          if (obj[paramName]) {
+            // convert value to array (if still string)
+            if (typeof obj[paramName] === 'string') {
+              obj[paramName] = [obj[paramName]];
+            }
+            // if no array index number specified...
+            if (typeof paramNum === 'undefined') {
+              // put the value on the end of the array
+              obj[paramName].push(paramValue);
+            }
+            // if array index number specified...
+            else {
+              // put the value at that index number
+              obj[paramName][paramNum] = paramValue;
+            }
+          }
+          // if param name doesn't exist yet, set it
+          else {
+            obj[paramName] = paramValue;
+          }
+        }
+      }
+    
+      return obj;
+    }
+    console.log(queryString)
+  // we'll store the parameters here
+  // var obj = {};
